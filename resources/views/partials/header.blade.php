1a9cf3c0935db25aaf8f97c94882fe23738895ae
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-08 16:02:01 +0100

    create route home on navbar dc logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="{{route('home')}}">
         <img src="{{Vite::asset('resources/assets/images/dc-logo.png')}}" alt="">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
