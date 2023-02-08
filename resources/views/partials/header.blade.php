a44d962701fc29e5ea7543c388cfcea0660a6534
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-08 15:55:40 +0100

    add logo to navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">DC Comics</a>
+    <a class="navbar-brand" href="#">
+        <img src="{{Vite::asset('resources/assets/images/dc-logo.png')}}" alt="">
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
