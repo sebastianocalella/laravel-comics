02d68bc765d57489d222f3e46634e2a51984e98e
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-02-08 16:54:15 +0100

    comic content prototype

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('main-content')
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+                <h2>
+                    titolo del fumetto
+                </h2>
+                <p>
+                    descrizione del fumetto
+                </p>
+            </div>
+            <div class="col-3">
+                <img src="" alt="advertisment">
+            </div>
+        </div>
+    </div>
 @endsection
